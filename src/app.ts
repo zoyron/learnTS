a3b018014504e3b5e39ab820d7b571043db4395f
zoyron <sgrarora3@gmail.com>
2024-08-29 18:07:15 +0530

    mixed type array

--- src/app.ts
+++ src/app.ts
@@ -32,3 +32,17 @@ let notAssigned: undefined = undefined;
 
 console.log(emptyValue);
 console.log(notAssigned);
+
+/**
+ * there are 2 ways to declare and array in tyepscript
+ * 1. let/const varName: TypeName [] = value...;
+ * 2. let/const varName: Array<TypeName> = value...;
+ *
+ * of the above two, 2nd method is used to declare other data structes apart from arrays as well
+ * let/const varName: DataStructureName<TypeName> = value...;
+ */
+
+// union type array
+const mixedArray: (string | number)[] = ["Sagar", 27, "Arora", "Coder"];
+
+console.log(mixedArray);
