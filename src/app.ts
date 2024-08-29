b99dc29a3bfdcbe9a35824f32249485795a7a435
zoyron <sgrarora3@gmail.com>
2024-08-29 17:58:14 +0530

    ts 7 done

--- src/app.ts
+++ src/app.ts
@@ -2,4 +2,33 @@ const message = "Hello, typescript!";
 
 console.log(message);
 
-console.log({message});
+console.log({ message });
+
+let isLoggedIn: boolean = true;
+let isEnabled: boolean = true;
+
+let numbers: number[] = [1, 2, 3];
+let names: string[] = ["Sagar", "Shubham", "Srishti"];
+
+function logMessages(message: string): void {
+  console.log(message);
+}
+
+logMessages("lolwa");
+
+let age: number = 30;
+let price: number = 19.99;
+console.log(age);
+console.log(price);
+
+let personName: string = "Sagar";
+let address: string = "240 Raja Garden";
+
+console.log(personName);
+console.log(address);
+
+let emptyValue: null = null;
+let notAssigned: undefined = undefined;
+
+console.log(emptyValue);
+console.log(notAssigned);
