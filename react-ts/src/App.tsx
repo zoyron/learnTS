8b6c97b840215d6c1499f493fff95aa6c56357f0
zoyron <sgrarora3@gmail.com>
2024-09-10 22:16:23 +0530

    minor update

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -1,9 +1,10 @@
+import React from "react";
 import "./App.css";
 
 export default function App(): React.ReactElement {
   return (
     <>
-      <h1>Typescript</h1>
+      <h1>mhgarments.com and tyreking.in</h1>
     </>
   );
 }
